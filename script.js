d8b89d3524194c7e3293bc52fbdb4065e1149ca2
VolodiaVrt <117577628+VolodiaVrt@users.noreply.github.com>
2023-06-10 14:10:42 +0300

    Update from laptop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let container=document.querySelector(".container");
 for (let i=0;i<=256;i++) {
     let newDiv=document.createElement("div");
+    newDiv.className="cell";
     container.appendChild(newDiv);
 }
